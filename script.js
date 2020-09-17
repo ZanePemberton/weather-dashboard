0ce10952c59021717d341ac23444b0b42d1fa07d
zane pemberton <zanepemberton@outlook.com>
2020-09-17 13:20:19 +1000

    added weather api

---
+++ script.js
@@ -0,0 +1,49 @@
+//getting current date
+let today = new Date();
+let date = (today.getDate() + 1) + '/' + today.getMonth() + '/' + today.getFullYear();
+let queryUrl;
+if (localStorage.getItem("last") !== null) {
+    queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${localStorage.getItem('last')},aus&units=metric&APPID=3b5715c4cc3d80c499e1192a073d5982`;
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        let iconUrl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+        let icon = $('<img>').attr('src', iconUrl);
+        let card = $('<div>').addClass('card mt-3 mx-3');
+        let body = $("<div>").addClass('card-body');
+        let head = $("<h5>").addClass('card-title').text(response.name + ' (' + date + ')').append(icon);
+        let temp = $("<p>").addClass('card-text').text('Temperature: ' + Math.round(response.main.temp) + '°C');
+        let humid = $("<p>").addClass('card-text').text('Humidity: ' + response.main.humidity + '%');
+        let wind = $("<p>").addClass('card-text').text('Wind Speed: ' + response.wind.speed + 'kph');
+        body.append(head, temp, humid, wind);
+        card.append(body);
+        $('#displayContainer').prepend(card);
+    })
+}
+//submit event for search area
+$('#search').submit(function (e) {
+    e.preventDefault();
+    let userInput = $('#userInput').val().trim();
+    localStorage.setItem('last', userInput)
+    queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userInput},aus&units=metric&APPID=3b5715c4cc3d80c499e1192a073d5982`;
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        let iconUrl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+        let icon = $('<img>').attr('src', iconUrl);
+        let card = $('<div>').addClass('card mt-3 mx-3');
+        let body = $("<div>").addClass('card-body');
+        let head = $("<h5>").addClass('card-title').text(response.name + ' (' + date + ')').append(icon);
+        let temp = $("<p>").addClass('card-text').text('Temperature: ' + Math.round(response.main.temp) + '°C');
+        let humid = $("<p>").addClass('card-text').text('Humidity: ' + response.main.humidity + '%');
+        let wind = $("<p>").addClass('card-text').text('Wind Speed: ' + response.wind.speed + 'kph');
+        body.append(head, temp, humid, wind);
+        card.append(body);
+        $('#displayContainer').prepend(card);
+    });
+    $("#userInput").val('');
+  })
